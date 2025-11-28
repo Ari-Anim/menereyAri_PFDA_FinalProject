d7b0bc2eba551a7f76507439ecf8657a13c99a59
mener <arimenerey@gmail.com>
2025-11-27 21:15:14 -0600

    Pseudo_Code

---
+++ src/interactive_background.py
@@ -0,0 +1,18 @@
+def main():
+    pass
+
+# TODO Load character frames
+# TODO Organzie Frames
+# TODO Implement background
+# TODO Make Background Customizeable 
+# TODO implement foreground
+# TODO init pygame
+# TODO set up conditional arguments for which character to use based on mouse input
+# TODO Set up movement of character and speed
+
+
+
+
+
+if __name__ == "__main__":
+    main()
