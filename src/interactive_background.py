46829d278d06e5bfec1c8395f35f0b1611ab44d7
mener <arimenerey@gmail.com>
2025-11-29 08:42:55 -0600

    Writing out variables that might be needed, alongside starting pieces of event loop and more pseudocode.

--- src/interactive_background.py
+++ src/interactive_background.py
@@ -5,16 +5,25 @@ import pygame
 
 
 
-class Character_Anim_idle():
-    pass
+class Character_Anim():
+    
+    def __init__(self, mouse_click, mpos_x, mpos_y, 
+                 mouse_motion, character_pos=(0,0)):
+        self.pos = character_pos
+        self.frame = self.load_frames
 
 
-class Character_Anim_right():
-    pass
 
+    def load_frames(self, mouse_click, mpos_x, mpos_y):
+        pass
+    
+    def movement(self, mouse_click, mpos_x, mpos_y, mouse_motion, pos):
+        #if mpos_y is below window look down
+        #if mpos_x is to the left side use left loop
+        #if mpos_x is to the right side use right loop
+        #if if mouse_motion = False use idle loop.
+        pass
 
-class Character_Anim_left():
-    pass
 
 
 class foreground():
@@ -28,6 +37,30 @@ class background():
 def main():
     pygame.init()
     pygame.display.set_caption("Interactive Background")
+    resolution = (1920, 1080)
+    screen = pygame.displayy.set_mode((resolution), pygame.RESIZABLE)
+    click = False
+    mpos_x = 0
+    mpos_y = 0
+    character_pos_x = 0
+    character_pos_y = 0
+    mouse_motion = False
+    running = True
+
+    while running:
+
+            for event in pygame.event.get():
+                if event.type == pygame.Quit:
+                      running = False
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                     click = True
+                if event.type == pygame.MOUSEBUTTONUP:
+                     click = False
+                if event.type == pygame.MOUSEMOTION:
+                     mouse_motion = True
+                     mpos_x, mpos_y = pygame.mouse.get_pos()
+            #Render
+            pygame.display.flip()
     pygame.quit()
     
 
@@ -37,6 +70,7 @@ def main():
 # TODO Make Background Customizeable 
 # TODO implement foreground
 # TODO init pygame
+# TODO Setup Animation Loops to be called in Game Loop.
 # TODO set up conditional arguments for which character to use based on mouse input
 # TODO Set up movement of character and speed
 
