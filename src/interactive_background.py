b69e3cad429504da7667371e832888e81ab5f908
mener <arimenerey@gmail.com>
2025-11-30 10:03:11 -0600

    changing to use multiple frame lists depending on action.

--- src/interactive_background.py
+++ src/interactive_background.py
@@ -12,22 +12,34 @@ class Character_Anim():
         self.pos = character_pos
         self.frame = pygame.Surface([1920,1080])
 
-        self.frames = []
-        self.frames.append(pygame.image.load("idle/jumping.png"))
-        self.frames.append(pygame.image.load("idle/landing.png"))
-        self.frames.append(pygame.image.load("idle/mouse_down.png"))
-        self.frames.append(pygame.image.load("idle/move.png"))
-        self.frames.append(pygame.image.load("idle/start_jump.png"))
-        self.frames.append(pygame.image.load("right/jumping.png"))
-        self.frames.append(pygame.image.load("right/landing.png"))
-        self.frames.append(pygame.image.load("right/mouse_down.png"))
-        self.frames.append(pygame.image.load("right/move.png"))
-        self.frames.append(pygame.image.load("right/start_jump.png"))
-        self.frames.append(pygame.image.load("left/jumping.png"))
-        self.frames.append(pygame.image.load("left/landing.png"))
-        self.frames.append(pygame.image.load("left/mouse_down.png"))
-        self.frames.append(pygame.image.load("left/move.png"))
-        self.frames.append(pygame.image.load("left/start_jump.png"))
+        self.idle_jump_frames = []
+        self.idle_frames.append(pygame.image.load("idle/start_jump.png"))
+        self.idle_frames.append(pygame.image.load("idle/jumping.png"))
+        self.idle_frames.append(pygame.image.load("idle/landing.png"))
+        self.idle_frames.append(pygame.image.load("idle/move.png"))
+
+
+        self.idle_loop_frames = []
+
+        self.right_jump_frames = []
+        self.right_frames.append(pygame.image.load("right/start_jump.png"))
+        self.right_frames.append(pygame.image.load("right/jumping.png"))
+        self.right_frames.append(pygame.image.load("right/landing.png"))
+        self.right_frames.append(pygame.image.load("right/move.png"))
+
+
+        self.right_move_frames = []
+
+        self.left_jump_frames = []
+        self.left_frames.append(pygame.image.load("left/start_jump.png"))
+        self.left_frames.append(pygame.image.load("left/jumping.png"))
+        self.left_frames.append(pygame.image.load("left/landing.png"))
+        self.left_frames.append(pygame.image.load("left/move.png"))
+
+
+        self.left_move_frames = []
+
+        
 
         self.current_frame = 0
         self.frame = self.sprites[self.current_frame]
@@ -35,20 +47,14 @@ class Character_Anim():
         self.rect = self.image.get_rect()
         self.rect = self.pos
 
-    def _idle(self, mouse_click, mpos_x, mpos_y):
-        pass
-
-    def _right():
-         pass
-    
-    def _left():
-         pass
-    
     def movement(self, mouse_click, mpos_x, mpos_y, mouse_motion, pos):
-        #if mpos_y is below window look down
+        #if mpos_y is outside window idle
         #if mpos_x is to the left side use left loop
         #if mpos_x is to the right side use right loop
         #if if mouse_motion = False use idle loop.
+        pass
+
+    def frame_pick(self, mouse_click):
         pass
 
 
@@ -95,7 +101,7 @@ def main():
     pygame.quit()
     
 
-# TODO Load character frames
+# TODO Load character left_
 # TODO Organzie Frames
 # TODO Implement background
 # TODO Make Background Customizeable 
