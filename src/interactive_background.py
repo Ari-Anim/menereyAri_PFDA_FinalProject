069d0187de51ab08300709e86ec60b9c9d55642b
mener <arimenerey@gmail.com>
2025-11-30 10:25:43 -0600

    rewrote frame loops to be action based, drew in new frames, added .convert_alpha() to all frames. changed Character_Anim() class functions to include, _detect_movement(), _detect_jump(), and _choose_frame().

--- src/interactive_background.py
+++ src/interactive_background.py
@@ -7,39 +7,45 @@ import pygame
 
 class Character_Anim():
     
-    def __init__(self, mouse_click, mpos_x, mpos_y, 
-                 mouse_motion, character_pos=(0,0)):
-        self.pos = character_pos
+    def __init__(self, pos=(0,0)):
+        self.pos = pos
         self.frame = pygame.Surface([1920,1080])
 
         self.idle_jump_frames = []
-        self.idle_frames.append(pygame.image.load("idle/start_jump.png"))
-        self.idle_frames.append(pygame.image.load("idle/jumping.png"))
-        self.idle_frames.append(pygame.image.load("idle/landing.png"))
-        self.idle_frames.append(pygame.image.load("idle/move.png"))
-
+        self.idle_frames.append(pygame.image.load("idle/start_jump.png").convert_alpha())
+        self.idle_frames.append(pygame.image.load("idle/jumping.png").convert_alpha())
+        self.idle_frames.append(pygame.image.load("idle/landing.png").convert_alpha())
+        self.idle_frames.append(pygame.image.load("idle/move.png").convert_alpha())
 
         self.idle_loop_frames = []
+        self.idle_loop_frames.append(pygame.image.load("Idle/idle_1.png").convert_alpha())
+        self.idle_loop_frames.append(pygame.image.load("Idle/idle_2.png").convert_alpha())
+        self.idle_loop_frames.append(pygame.image.load("Idle/idle_3.png").convert_alpha())
+        self.idle_loop_frames.append(pygame.image.load("Idle/idle_4.png").convert_alpha())
 
         self.right_jump_frames = []
-        self.right_frames.append(pygame.image.load("right/start_jump.png"))
-        self.right_frames.append(pygame.image.load("right/jumping.png"))
-        self.right_frames.append(pygame.image.load("right/landing.png"))
-        self.right_frames.append(pygame.image.load("right/move.png"))
-
+        self.right_frames.append(pygame.image.load("right/start_jump.png").convert_alpha())
+        self.right_frames.append(pygame.image.load("right/jumping.png").convert_alpha())
+        self.right_frames.append(pygame.image.load("right/landing.png").convert_alpha())
+        self.right_frames.append(pygame.image.load("right/right_1.png").convert_alpha())
 
         self.right_move_frames = []
+        self.right_move_frames.append(pygame.image.load("right/right_1.png").convert_alpha())
+        self.right_move_frames.append(pygame.image.load("right/right_2.png").convert_alpha())
+        self.right_move_frames.append(pygame.image.load("right/right_3.png").convert_alpha())
+        self.right_move_frames.append(pygame.image.load("right/right_4.png").convert_alpha())
 
         self.left_jump_frames = []
-        self.left_frames.append(pygame.image.load("left/start_jump.png"))
-        self.left_frames.append(pygame.image.load("left/jumping.png"))
-        self.left_frames.append(pygame.image.load("left/landing.png"))
-        self.left_frames.append(pygame.image.load("left/move.png"))
-
+        self.left_frames.append(pygame.image.load("left/start_jump.png").convert_alpha())
+        self.left_frames.append(pygame.image.load("left/jumping.png").convert_alpha())
+        self.left_frames.append(pygame.image.load("left/landing.png").convert_alpha())
+        self.left_frames.append(pygame.image.load("left/left_1.png").convert_alpha())
 
         self.left_move_frames = []
-
-        
+        self.left_move_frames.append(pygame.image.load("left/left_1.png").convert_alpha())
+        self.left_move_frames.append(pygame.image.load("left/left_2.png").convert_alpha())
+        self.left_move_frames.append(pygame.image.load("left/left_3.png").convert_alpha())
+        self.left_move_frames.append(pygame.image.load("left/left_4.png").convert_alpha())
 
         self.current_frame = 0
         self.frame = self.sprites[self.current_frame]
@@ -47,17 +53,18 @@ class Character_Anim():
         self.rect = self.image.get_rect()
         self.rect = self.pos
 
-    def movement(self, mouse_click, mpos_x, mpos_y, mouse_motion, pos):
+    def _detect_movement(self, mouse_click, mpos_x, mpos_y, mouse_motion, pos):
         #if mpos_y is outside window idle
         #if mpos_x is to the left side use left loop
         #if mpos_x is to the right side use right loop
-        #if if mouse_motion = False use idle loop.
+        #if mouse_motion = False use idle loop.
         pass
 
-    def frame_pick(self, mouse_click):
+    def _detect_jump(self, mouse_click):
         pass
 
-
+    def choose_frame(self):
+        pass
 
 class foreground():
     pass
